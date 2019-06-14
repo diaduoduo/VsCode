a6bce69c40dc9a06ee5d87601772ea76d0bcfa1d
diaduoduo <940063189@qq.com>
2019-06-14 09:11:07 +0800

    视频教程

--- @vuecli-demo2/demo/src/views/vueadmin.vue
+++ @vuecli-demo2/demo/src/views/vueadmin.vue
@@ -7,7 +7,12 @@
        <mt-button type="default">default</mt-button>
         <mt-button type="primary">primary</mt-button>
         <mt-button type="danger">danger</mt-button>
-        <todo/>
+<!--         <todo/> -->
+
+
+           <input type="button" value ="get请求" @click="getInfo">
+    <input type="button" value="post请求" @click="postInfo">
+    <input type="button" value="jsonp请求" @click="jsonpInfo">
     </div>
 </template>
 
@@ -19,6 +24,24 @@
                 msg:"dddddd"
             }
         },
+        methods: {
+            getInfo(){
+                this.$http.get("http://vue.studyit.io/api/gitlunbo").then(function(result){
+                    console.log(result)
+                })
+            },
+            postInfo(){
+                this.$http.post("http://vue.studyit.io/api/gitlunbo",{},{emulateJSON:true}).then(result =>{
+                    console.log(result)
+                })
+            },
+            jsonpInfo(){
+                this.$http.jsonp('http://vue.studyit.io/api/gitlunbo').then(result =>{
+                    console.log(result)
+                })
+                
+            }
+        }
     }
 </script>
 
