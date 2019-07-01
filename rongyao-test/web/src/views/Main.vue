e050a19b2e9ca47d28e6604edf14c54729421b85
diaduoduo <940063189@qq.com>
2019-07-01 11:02:46 +0800

    重新上传

---
+++ rongyao-test/web/src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="topbar bg-black py-2 text-white">
+            topbar
+        </div>
+        <div class="nav">
+sdf
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
